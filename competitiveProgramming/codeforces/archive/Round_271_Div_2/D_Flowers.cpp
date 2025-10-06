f2e410b0a6d2884cd1beeeb8c91d7e3a0c71ec6c
Glenn <edstrom.glenn@gmail.com>
2025-10-06 13:54:28 -0500

    desktop

--- competitiveProgramming/codeforces/archive/Round_271_Div_2/D_Flowers.cpp
+++ competitiveProgramming/codeforces/archive/Round_271_Div_2/D_Flowers.cpp
@@ -34,82 +34,66 @@ typedef long long ll;
 // RWWWW
 
 const ll MOD = 1e9+7;
+//const ll UPPERCHOOSE = 1e5+1;
 
 vector<ll> fact = {1,1};
-ll factorial(int n){
-    for(int i = fact.size(); i <= n; i++){
+ll factorial(ll n){
+    for(ll i = fact.size(); i <= n; i++){
         fact.push_back(fact[(fact.size()-1)]*i%MOD);
     }
     return fact[n];
 }
 
-vector<vector<ll>> nCk(100, vector<ll>(100,0));
-ll choose(int n, int k){
+ll mod_inv(ll n){
+    // compute n^(mod-2) % mod
+
+    return 1;
+}
+
+//vector<vector<ll>> nCk(UPPERCHOOSE, vector<ll>(UPPERCHOOSE,0));
+ll choose(ll n, ll k){
     assert(n>=k);
-    return nCk[n][k];
+    return factorial(n) * mod_inv(factorial(k)) * mod_inv(factorial(n-k));
 }
 
-void pascal(){
-    // BAD because factorial already mods the number, can't divide a modded num
-
-    // initialize first column to 1
-    for(int i = 0; i < nCk.size(); i++){ nCk[i][0] = 1; }
-
-    for(int i = 1; i < nCk.size(); i++){
-        for(int j = 1; j <= i; j++){
-            nCk[i][j] = (nCk[i-1][j-1] + nCk[i-1][j]) % MOD;
-        }
-    }
-    //for(int i = 0; i < 100; i++){
-    //    for(int k = 0; k <= i; k++){
-    //        nCk[i][k] = factorial(i)/factorial(k)/factorial(i-k);
-    //    }
-    //}
-    // dp[0] =     {1}
-    // dp[1] =    {1,1}
-    // dp[2] =   {1,2,1}
-    // dp[3] =  {1,3,3,1}
-    // dp[3] = {1,4,6,4,1}
-    //
-    // what's this dp transition like?
-    //
-    // dp[i][0] = 1; for all i
-    //
-    // for j in range(len(prev+1))
-    // dp[i][j] = dp[i][]
-}
+//void pascal(){
+//    // BAD because factorial already mods the number, can't divide a modded num
+//    // initialize first column to 1
+//    for(ll i = 0; i < nCk.size(); i++){ nCk[i][0] = 1; }
+//
+//    for(ll i = 1; i < nCk.size(); i++){
+//        for(ll j = 1; j <= i; j++){
+//            nCk[i][j] = (nCk[i-1][j-1] + nCk[i-1][j]) % MOD;
+//        }
+//    }
+//}
 
 ll k;
 void precalc(){
-    pascal();
+    //pascal();
     cin >> k;
 }
 
 void solve() {
     ll a,b; cin >> a >> b;
-    for(auto& row : nCk){
-        for(auto& o : row) if(o != 0){
-            cout << o << ' ';
-        }
-        cout << endl;
-    }
-    return;
+    //for(auto& row : nCk){
+    //    for(auto& o : row) if(o != 0){
+    //        cout << o << ' ';
+    //    }
+    //    cout << endl;
+    //}
 
-    // step 1: get all multiples of k in the range from 0 to a to b
-    // step 2: sum the range of red flowers.
-    //
-    // let W = number of white flowers
-    // let x = b - W
-    // tot += x*(x+1)/2
+    // for every value that satisfies R + W*k == X
+    // where X ∈ [l,r]
+
     ll tot = 0;
-    for(ll W = 0; W <= b; W+=k){ // white flower values
-        ll x = b - W;
-        tot += x*(x+1)/2 % (ll)(1e9+7);
-        //ll redlow = max(a,W)-W; // how many red flowers on the lower end
-        //ll redhigh = b-k;
-        // white is i
-        // red is everything from max(a,i)-k to b-k
-
+    for(ll X = a; X <= b; X++){
+        for(ll W = 0; W*k <= X; W++){ // white flower values
+            ll R = X - W*k;
+            ll n = R + W;
+            //cout << X << "," << W << choose(X,R) << endl;
+            tot = (tot + choose(n,W)) % MOD;
+        }
     }
     cout << tot << endl;
 
